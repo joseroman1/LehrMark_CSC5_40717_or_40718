7715f0293a84cedaeb1dea1d1c6a63274f6edc7e
ml1150258 <mark.lehr@rcc.edu>
2015-01-07 11:10:35 -0800

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Class_Lab/HelloWorld/main.cpp
+++ Class_Lab/HelloWorld/main.cpp
@@ -3,7 +3,7 @@
    Author: Dr. Mark E. Lehr
    Created on January 6, 2015, 6:05 PM
    Purpose: Our first program
-            I am up at Github now!!!!
+            This is my latest change
  */
 
 //System Libraries
