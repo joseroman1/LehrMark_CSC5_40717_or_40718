2b2772d272d6fef68e9aad703229d73e6be61e65
ml1150258 <mark@omniplatform.com>
2015-01-08 08:52:23 -0800

    Signed-off-by: ml1150258 <mark@omniplatform.com>

---
+++ Hmwk/Assignment1/Savitch_9thEd_Chap2_ProgProj5/main.cpp
@@ -0,0 +1,36 @@
+/*
+* File: main.cpp
+* Author: Haolan Ye(Benjamin)
+* Created on January 8, 2015, 12:28 AM
+* Purpose: Homework assignment (Savitch_8thEd_Chap1_ProgProj10)
+ *       Modified by Mark E. Lehr
+*/
+//System Libraries
+#include <iostream>
+using namespace std;
+//User Libraries
+//Global Constants
+//Function Prototypes
+//Execution begins here
+int main(int argc, char** argv) {
+//Declare variables
+char c;//character
+//Prompt user for a character
+cout << "This program will output a large block letter \"C\""<<endl;
+cout << "With a character you input." << endl;
+cout << "Input a character "<< endl;
+cin >> c;
+cout << endl << endl;
+//Output a large letter "C" with a character user enter
+cout << "  " << c << c << c << c << c<< endl;
+cout << c << c << "     " << c << c << endl;
+cout << c << endl;
+cout << c << endl;
+cout << c << endl;
+cout << c << endl;
+cout << c << endl;
+cout << c << c << "     " << c << c << endl;
+cout << "  " << c << c << c << c << c<< endl;
+//Exit stage right
+return 0;
+}
